89304826885c2843f5ca314205b26b15183808bb
Bruno Fernandes Horn <59888303+brunofhorn@users.noreply.github.com>
2025-05-27 21:06:00 -0300

    change middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,29 +5,6 @@ import jwt from 'jsonwebtoken';
 const JWT_TOKEN_SECRET = process.env.NEXT_JWT_TOKEN_SECRET!;
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('token')?.value;
-
-  console.log(token)
-
-  const isAuth = !!token && (() => {
-    try {
-      jwt.verify(token, JWT_TOKEN_SECRET);
-      return true;
-    } catch {
-      return false;
-    }
-  })();
-
-  const isLoginPage = request.nextUrl.pathname === '/';
-
-  if (!isAuth && !isLoginPage) {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
-
-  if (isAuth && isLoginPage) {
-    return NextResponse.redirect(new URL('/dashboard/videos', request.url));
-  }
-
   return NextResponse.next();
 }
 
